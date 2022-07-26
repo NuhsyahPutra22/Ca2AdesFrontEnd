543bef96fb61940539f08fdfe1c945364b7c93a6
Beh Pei Hao <haobeh0118@gmail.com>
2022-07-26 17:10:49 +0800

    update login.js(done)

--- js/my-login.js
+++ js/my-login.js
@@ -2,6 +2,8 @@ const isLocalhost = location.hostname === 'localhost' || location.hostname === '
 const STORAGE_API_HOST = isLocalhost ? `http://localhost:3000` : `https://keyval-store.herokuapp.com`;
 
 
+const notificationtext=document.getElementById("loginform")
+
 // document.getElementById("")
 
 window.addEventListener("DOMContentLoaded", function () {
@@ -10,33 +12,68 @@ window.addEventListener("DOMContentLoaded", function () {
         var loginuser = document.getElementById("loginform");
         loginuser.addEventListener("submit", function(event) {
             event.preventDefault();
-            var username = document.getElementById("email").value;
-            var userpassword = document.getElementById("password").value;
+            const requestBody= getvalue()
 
-            fetch(`http://localhost:3000/login`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({username,userpassword}),
-              })
+          if (requestBody.username===''){
+            text+='Username';
+            loginuser.innerHTML=text;
+            if (requestBody.userpassword===''){
+              text+='&Password';
+              loginuser.innerHTML=text
+            }
+          }
+
+          else if(requestBody.userpassword===''){
+            text+='Password';
+            loginuser.innerHTML=text
+          }
+
+          else{
+            text='';
+            loginuser.innerHTML=text
+
+
+          
+axios.post(`http://localhost:3000/login`,requestBody)      
               .then((res) => {
-            
-                // console.log(response.json())
                 if (res.status === 200) {
                     console.log("Success!");
-                    // console.log(token)
-                    // sessionStorage.setItem("jwt", response.data.jwtToken);
-                    res.json()
                     console.log(res)
                     window.location.href = "../view/Module.html";
+                    return res.data
                 } else {
                   res.json().then((error) => { throw error });
                 }
               })
-              .catch((error) => alert(error.message))
-             
 
+              .then((data)=>{
+                sessionStorage.setItem("token",data.token)
+                sessionStorage.setItem("username",data.username)
+                sessionStorage.setItem("userpassword",data.userrole)
+              })
+              .catch((error) => alert(error.message))
+           
+            }
 
         })
 })
+
+
+function getvalue(){
+  const username = document.getElementById("email").value;
+  const userpassword = document.getElementById("password").value;
+const requestBody ={
+  username:username,
+  userpassword:userpassword
+}
+return requestBody
+}
+
+
+
+
+
+
+
+
+
