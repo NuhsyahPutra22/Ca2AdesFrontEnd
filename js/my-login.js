7171f703614d377a637de813111a4528dea325e4
Beh Pei Hao <haobeh0118@gmail.com>
2022-07-26 15:26:52 +0800

    create module.js

--- js/my-login.js
+++ js/my-login.js
@@ -2,7 +2,7 @@ const isLocalhost = location.hostname === 'localhost' || location.hostname === '
 const STORAGE_API_HOST = isLocalhost ? `http://localhost:3000` : `https://keyval-store.herokuapp.com`;
 
 
-document.getElementById("")
+// document.getElementById("")
 
 window.addEventListener("DOMContentLoaded", function () {
 
@@ -13,22 +13,25 @@ window.addEventListener("DOMContentLoaded", function () {
             var username = document.getElementById("email").value;
             var userpassword = document.getElementById("password").value;
 
-            fetch(`${STORAGE_API_HOST}/login`, {
+            fetch(`http://localhost:3000/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({username,userpassword}),
               })
-              .then((response) => {
-                if (response.status === 200) {
+              .then((res) => {
+            
+                // console.log(response.json())
+                if (res.status === 200) {
                     console.log("Success!");
                     // console.log(token)
                     // sessionStorage.setItem("jwt", response.data.jwtToken);
-                    console.log(response)
-                    window.location.href = "./Gpa-calculator.html";
+                    res.json()
+                    console.log(res)
+                    window.location.href = "../view/Module.html";
                 } else {
-                  response.json().then((error) => { throw error });
+                  res.json().then((error) => { throw error });
                 }
               })
               .catch((error) => alert(error.message))
