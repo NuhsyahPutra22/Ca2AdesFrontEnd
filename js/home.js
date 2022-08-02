5966261e18948fee0182ab9afdcaf9e5af7850c0
Beh Pei Hao <haobeh0118@gmail.com>
2022-08-02 22:11:36 +0800

    modify home.js

--- js/home.js
+++ js/home.js
@@ -2,43 +2,43 @@ var a=sessionStorage.getItem("userrole")
 var b=sessionStorage.getItem("userid")
 // var courseid;
 
-if (a=="Admin"){
-    const  html=`
-    <select  id="modes">
-    <option value="Admin">Admin mode</option>
-    <option value="Student">Student Mode</option>
+// if (a=="Admin"){
+//     const  html=`
+//     <select  id="modes">
+//     <option value="Admin">Admin mode</option>
+//     <option value="Student">Student Mode</option>
     
-  </select>`
+//   </select>`
 
 
-document.getElementById("changemode").innerHTML=html;
+// document.getElementById("changemode").innerHTML=html;
 
 
-if (document.getElementById("modes").value=="Admin"){
-const posthtml=`     <nav class="navbar navbar-expand-sm">
-<div class="container-fluid">
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" href="/view/AdminIndex.html">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="/view/AdminModule.html">Modules</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link active" href="/view/AminCourse.html">Courses</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="/view/Gpa-calculator.html">Target Setting</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="/view/AdminFeedback.html">Feedback From Student</a>
-    </li>
-  </ul>
-</div>
-</nav>`
-document.getElementById("navbar").innerHTML=posthtml
-}
-}
+// if (document.getElementById("modes").value=="Admin"){
+// const posthtml=`     <nav class="navbar navbar-expand-sm">
+// <div class="container-fluid">
+//   <ul class="navbar-nav">
+//     <li class="nav-item">
+//       <a class="nav-link" href="/view/AdminIndex.html">Home</a>
+//     </li>
+//     <li class="nav-item">
+//       <a class="nav-link" href="/view/AdminModule.html">Modules</a>
+//     </li>
+//     <li class="nav-item">
+//       <a class="nav-link active" href="/view/AminCourse.html">Courses</a>
+//     </li>
+//     <li class="nav-item">
+//       <a class="nav-link" href="/view/Gpa-calculator.html">Target Setting</a>
+//     </li>
+//     <li class="nav-item">
+//       <a class="nav-link" href="/view/AdminFeedback.html">Feedback From Student</a>
+//     </li>
+//   </ul>
+// </div>
+// </nav>`
+// document.getElementById("navbar").innerHTML=posthtml
+// }
+// }
 
 
 
@@ -72,6 +72,7 @@ sessionStorage.setItem("courseid",courseid)
               <h2>About Me</h2>
               <p>USERID: ${data.result[i].username}</p>
               <p>USEREMAIL: ${data.result[i].useremail}</p>
+              <p>semester: ${data.result[i].semestername}</p>
               <p>CONTACT NUMBER:  ${data.result[i].usercontactnumber}</p>
               <p>ADDRESS:  ${data.result[i].useraddress}</p>
               </div>
@@ -91,7 +92,7 @@ sessionStorage.setItem("courseid",courseid)
 )
 .catch((error) => alert(error.message))
 const courseid=sessionStorage.getItem("courseid")
-axios.get(`http://localhost:3000/Modulecourse/`+courseid)      
+axios.get(`http://localhost:3000/ModulebyUser/`+b)      
 .then((res) => {
   if (res.status === 200) {
       console.log("Success!");
@@ -104,12 +105,14 @@ axios.get(`http://localhost:3000/Modulecourse/`+courseid)
 
 .then((data)=>{
 
-
-  for (let i = 0; i < data.result.length; i++) {
-      const posthtml=`
-      <h5>${data.result[i].modulecode} ${data.result[i].modulename}</h5>
-      
-      `
+      var posthtml=`<div>`
+      for (let i = 0; i < data.result.length; i++) {
+        
+        posthtml+=`
+        <h5>${data.result[i].modulecode} ${data.result[i].modulename}</h5>
+        
+        `
+         posthtml+=`</div>`
        
         
           document.getElementById("module").innerHTML=posthtml
